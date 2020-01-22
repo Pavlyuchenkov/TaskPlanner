12cf6062e28359db1042f36db02eaa18cb66bf45
Pavlyuchenkov <pavluchenkov.vladimir@mail.ru>
2020-01-22 16:30:30 +0300

    1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TaskPlanner.Models;
+
+namespace TaskPlanner.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult Create(Models.Task task)
+        {
+            List<Models.Task> tasks = new List<Models.Task>
+            {
+                new Models.Task{TaskId = task.TaskId, TaskTime = DateTime.Now, TaskName = task.TaskName}
+            };
+            tasks.Add(task);
+            return View("Index", tasks);
+        }
+
+        public ActionResult GetMessage()
+        {
+            return PartialView("_GetMessage");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskPlanner.Models
+{
+    public class Task
+    {
+        public int TaskId { get; set; } //used
+        public string TaskName { get; set; } //used
+        public string TaskDescrip { get; set; }
+        //время постановки задачи
+        public DateTime TaskTime { get; set; } //used
+        //время выполнения задачи
+        public DateTime EndTime { get; set; }
+        //время затраченное на выполнение задачи
+        public DateTime DifTime { get; set; }
+    }
+}
