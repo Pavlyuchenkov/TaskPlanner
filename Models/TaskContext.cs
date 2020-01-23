3d8e01dedc5b66a4efaddc62d883dcea3237841a
Pavlyuchenkov <pavluchenkov.vladimir@mail.ru>
2020-01-23 16:05:26 +0300

    1

---
+++ Models/TaskContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskPlanner.Models
+{
+    public class TaskContext : DbContext
+    {
+        public DbSet<Task>Tasks { get; set; }
+        public TaskContext(DbContextOptions<TaskContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TaskPlanner.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace TaskPlanner
 {
@@ -23,6 +25,9 @@ namespace TaskPlanner
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<TaskContext>(options =>
+                options.UseSqlServer(connection));
             services.AddControllersWithViews();
         }
 
